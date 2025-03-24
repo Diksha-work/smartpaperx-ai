e86c42a68fad0e1f5b81cbb15fd5990719314c0f
Diksha-work <diksha.annamalai.work@gmail.com>
2025-03-24 18:54:04 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,7 @@
 
 // Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyBJynUCGqWc6QUQQICv6OZEzy0XrDtXwAc",
-  authDomain: "dynamic-question-paper.firebaseapp.com",
-  projectId: "dynamic-question-paper",
-  storageBucket: "dynamic-question-paper.firebasestorage.app",
-  messagingSenderId: "679356827844",
-  appId: "1:679356827844:web:299d3edd0c403d99ffa191",
-  measurementId: "G-TN8GWXRBJZ"
+enter api thing here
 };
 
 export { firebaseConfig };
