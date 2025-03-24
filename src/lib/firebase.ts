580f8ecc26dbc1b9322e1da8d252ab06560579a4
Diksha-work <diksha.annamalai.work@gmail.com>
2025-03-24 18:52:28 +0530

    firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,13 @@
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeBRq1z-diZGRuxSonsI4Jn-9UQ8v8IU0",
-  authDomain: "aptora-4108d.firebaseapp.com",
-  projectId: "aptora-4108d",
-  storageBucket: "aptora-4108d.firebasestorage.app",
-  messagingSenderId: "966897977550",
-  appId: "1:966897977550:web:8f8910927011ab9225689f",
-  measurementId: "G-SWFKT9VK3P"
+apiKey: "AIzaSyBJynUCGqWc6QUQQICv6OZEzy0XrDtXwAc",
+  authDomain: "dynamic-question-paper.firebaseapp.com",
+  projectId: "dynamic-question-paper",
+  storageBucket: "dynamic-question-paper.firebasestorage.app",
+  messagingSenderId: "679356827844",
+  appId: "1:679356827844:web:299d3edd0c403d99ffa191",
+  measurementId: "G-TN8GWXRBJZ"
 };
 
 export { firebaseConfig };
